cec837ea85ff67cdf24b07627361ba93cba390ab
TziviMaklev <tm0527171162@gmail.com>
2024-05-29 20:00:39 +0300

    starte route

---
+++ Server/Routes/api.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const cors = require('cors');
+const carsRouter = require('../Controllers/cars');
+const furnitureRouter = require('../Controllers/furniture');
+const appliancesRouter = require('../Controllers/appliances');
+const animalsRouter = require('../Controllers/animals');
+const businessesRouter = require('../Controllers/businesses');
+const usersRouter = require('../Controllers/users');
+const userMessagesRouter = require('../Controllers/userMessages');
+const reservedAdsRouter = require('../Controllers/reservedAds');
+
+
+
+
+const PORT=3000;
+
+const server=express();
+
+server.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
+}));
+
+server.use(express.json());
+
+server.use('/cars', carsRouter);
+server.use('/furniture', furnitureRouter);
+server.use('/appliances', appliancesRouter);
+server.use('/animals', animalsRouter);
+server.use('/businesses', businessesRouter);
+server.use('/users', usersRouter);
+server.use('/userMessages', userMessagesRouter);
+server.use('/reservedAds', reservedAdsRouter);
+
+
+
+server.listen(PORT, () => {
+    console.log(`Listening to requests at http://localhost:${PORT}`);
+});
